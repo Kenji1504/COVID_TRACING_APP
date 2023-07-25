0c468acca48c5455fd0ce6809c60837eff299a21
Kenji1504 <gamboakenleam1015@gmail.com>
2023-07-25 19:27:59 +0800

    to make user enter their vaccination status

--- main_app.py
+++ main_app.py
@@ -3,6 +3,7 @@
 # Creating a COVID-19 Contact Tracing App
 
 from tkinter import *
+from tkinter import ttk
 # Create a UI class that asks for user's contact info
 class GUI:
     def __init__(self):
@@ -12,7 +13,7 @@ class GUI:
         self.__main_window.columnconfigure(0, weight=1)
         self.__main_window.rowconfigure(0, weight=1)
 
-        TITLE = Label(self.__frame, text="COVID-19 Contact Tracing App", font= ('Arial Narrow', 18))
+        TITLE = Label(self.__frame, text="COVID-19 Contact Tracing App", font= ('Bauhaus 93', 23))
         TITLE.grid(row=0, column=0, columnspan=2, pady=10)
 
         self.__entries = []
@@ -30,11 +31,16 @@ class GUI:
         email_address = user_entry(self.__frame, 'Email: ')
         self.__entries.append(email_address)
         # Ask for user's vaccination status
+        vaccination_label = Label(self.__frame, text="Vaccination Status: ", font= ('OCR A Extended', 12))
+        vaccination_label.grid(row=self.__frame.grid_size()[1], column=0, pady=10)
+        vaccination_status = ttk.Combobox(self.__frame, values=["None", "1st Dose", "2nd Dose", "1st Booster Shot", "Second Booster Shot"], width = 37)
+        vaccination_status.grid(row=self.__frame.grid_size()[1] - 1, column=1, pady=10)
+        self.__entries.append(vaccination_status)
         
         self.__main_window.mainloop()
         
 def user_entry(master, user_name):
-    entry_label = Label(master, text=user_name, font= ('Arial Narrow', 12))
+    entry_label = Label(master, text=user_name, font= ('OCR A Extended', 12))
     entry = Entry(master, width = 40)
     entry_label.grid(row=master.grid_size()[1], column=0)
     entry.grid(row=master.grid_size()[1] - 1, column=1)
