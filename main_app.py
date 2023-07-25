1885ae184da1cc715a3704c153d028352ccf8077
Kenji1504 <gamboakenleam1015@gmail.com>
2023-07-25 17:07:44 +0800

    Pseudcode

---
+++ main_app.py
@@ -0,0 +1,11 @@
+# Ken Leam G. Gamboa
+# BSCPE 1-5
+# Creating a COVID-19 Contact Tracing App
+
+# Create a UI class that asks for user's contact info
+    # Ask for user's name
+    # Ask for user's age
+    # Ask for user's address
+    # Ask for user's email address
+    # Ask for user's vaccination status
+# Create a UI class that searches for user's inputted info
