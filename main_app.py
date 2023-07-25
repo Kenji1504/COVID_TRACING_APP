10d43899d8e69526e7c2b45b27c0c911d657479d
Kenji1504 <gamboakenleam1015@gmail.com>
2023-07-25 17:33:42 +0800

    creating a window for UI

--- main_app.py
+++ main_app.py
@@ -2,10 +2,23 @@
 # BSCPE 1-5
 # Creating a COVID-19 Contact Tracing App
 
+from tkinter import *
 # Create a UI class that asks for user's contact info
-    # Ask for user's name
-    # Ask for user's age
-    # Ask for user's address
-    # Ask for user's email address
-    # Ask for user's vaccination status
+class GUI:
+    def __init__(self):
+        self.__main_window = Tk()
+        TITLE = Label(self.__main_window, text="COVID-19 Contact Tracing App", font= ('Arial Narrow', 20))
+        TITLE.pack()
+
+        # Ask for user's name
+        # Ask for user's age
+        # Ask for user's address
+        # Ask for user's email address
+        # Ask for user's vaccination status
 # Create a UI class that searches for user's inputted info
+
+        
+        
+        
+        self.__main_window.mainloop()
+GUI()
