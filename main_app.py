7c8667b3512788b0e871c4fae374e95d0d8fe0c9
Kenji1504 <gamboakenleam1015@gmail.com>
2023-07-25 19:48:53 +0800

    for user to save their entry

--- main_app.py
+++ main_app.py
@@ -36,9 +36,28 @@ class GUI:
         vaccination_status = ttk.Combobox(self.__frame, values=["None", "1st Dose", "2nd Dose", "1st Booster Shot", "Second Booster Shot"], width = 37)
         vaccination_status.grid(row=self.__frame.grid_size()[1] - 1, column=1, pady=10)
         self.__entries.append(vaccination_status)
+
+        # Make an add button 
+        add_button = Button(self.__frame, text="Add Entry", font= ('Franklin Gothic Heavy', 12), width= 20, command=self.__add_entry)
+        add_button.grid(row=self.__frame.grid_size()[1], column=0, columnspan=2, pady=10)
+        
         
         self.__main_window.mainloop()
-        
+    # Append the entered values
+    def __add_entry(self):
+
+        entry_values = []
+        for entry in self.__entries:
+            entry_value = entry.get()
+            entry_values.append(entry_value)
+
+            entry.delete(0, END)
+
+        contact_to_save = ";".join(entry_values)
+
+        with open("log_file.txt", "a") as contacts_file:
+            contacts_file.write(f"{contact_to_save}\n")
+
 def user_entry(master, user_name):
     entry_label = Label(master, text=user_name, font= ('OCR A Extended', 12))
     entry = Entry(master, width = 40)
