c8a5c184c06e8cad4f2b90bfb46d7592841991ce
Kenji1504 <gamboakenleam1015@gmail.com>
2023-07-26 22:44:32 +0800

    organizing the inputted info from text box

--- output_window.py
+++ output_window.py
@@ -9,5 +9,16 @@ class OutputWindow:
         self.__frame.pack()
 
         self.__label = Label(self.__frame, text="Search Results", font=('Bauhaus 93', 12))
-        self.__label.pack()
-        
+        self.__label.grid(row=0, column=0, columnspan=5)
+
+        self.__contacts_label = Label(self.__frame, padx=10, pady=10)
+
+        contacts.insert(0, "Name;Age;Address;Email Address;Vaccination Status")
+
+        for idx, contact in enumerate(contacts):
+            attributes = contact.split(";")
+
+            for index, attribute in enumerate(attributes):
+
+                attribute_label = Label(self.__frame, text=attribute)
+                attribute_label.grid(row=idx+1, column=index)
